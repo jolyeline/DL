0a7eca2db1e58e86c6228b32e0651553f373beae
MSE Morgan <74973573+msemorg@users.noreply.github.com>
2026-05-06 12:33:52 +0200

    added Axis description + Loss value return

--- src/model_lstm.py
+++ src/model_lstm.py
@@ -84,7 +84,7 @@ recursive_preds_scaled = []
 
 current_batch = last_window
 for _ in range(PREDICTIONS):
-    pred = best_model.predict(current_batch, verbose=0)
+    pred = best_model.predict(current_batch, verbose=1)
     recursive_preds_scaled.append(pred[0, 0])
     current_batch = np.append(current_batch[:, 1:, :], pred.reshape(1,1,1), axis=1)
 
@@ -92,6 +92,8 @@ recursive_preds = scaler.inverse_transform(np.array(recursive_preds_scaled).resh
 
 plt.figure(figsize=(10, 5))
 plt.plot(best_history.history['loss'], label='Training Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
 plt.legend()
 plt.savefig("output/loss_lstm.png")
 plt.show()
@@ -99,6 +101,11 @@ plt.show()
 plt.figure(figsize=(12, 6))
 plt.plot(range(len(X_raw)), X_raw, label="History")
 plt.plot(range(len(X_raw), len(X_raw)+PREDICTIONS), recursive_preds, label="Forecast")
+plt.xlabel('Time Steps')
+plt.ylabel('Value')
 plt.legend()
 plt.savefig("output/forecast_results_lstm.png")
 plt.show()
+
+final_loss = best_history.history['loss'][-1]
+print("Final Training Loss:", final_loss)
